2789065d630c9f79fcb35c89c23778bed0dda146
daniel raffel <daniel.raffel@gmail.com>
2023-11-20 02:45:04 -0800

    updated main.py

--- sonicgarbage/main.py
+++ sonicgarbage/main.py
@@ -14,10 +14,6 @@ app = Flask(__name__)
 # Base directory on the server (modify this path as needed)
 base_dir = '/var/www/audio'
 
-# Check if base directory exists, if not, create it
-if not os.path.exists(base_dir):
-    os.makedirs(base_dir)
-
 # Function to create a timestamped folder
 def create_timestamped_folder():
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
@@ -25,7 +21,6 @@ def create_timestamped_folder():
     os.makedirs(path, exist_ok=True)
     return path
 
-# Use the timestamped folder for this run
 current_run_dir = create_timestamped_folder()
 
 # Paths for the folders within the timestamped folder
@@ -50,78 +45,131 @@ if not os.path.exists(word_list_file):
         for word in word_list:
             file.write(word + '\n')
 
-# Set BATCH_SIZE to 45
-BATCH_SIZE = 45
-MAX_SEARCH_RESULTS = 10
-DOWNLOAD_DIR = raw_dir
-LOOP_OUTPUT_DIR = loop_dir
-ONESHOT_OUTPUT_DIR = oneshot_dir
-WORD_LIST = word_list_file
+# Functions from the original notebook for audio processing
+def read_lines(file):
+    return open(file).read().splitlines()
 
-# Function to generate HTML for audio files
-def generate_html_for_audio_files(directory):
-    html_content = ""
-    files = sorted([f for f in os.listdir(directory) if f.endswith('.wav')])
-    file_count = 0
+class download_range_func:
+    def __init__(self):
+        pass
+    def __call__(self, info_dict, ydl):
+        timestamp = self.make_timestamp(info_dict)
+        yield {
+            'start_time': timestamp,
+            'end_time': timestamp,
+        }
+    @staticmethod
+    def make_timestamp(info):
+        duration = info['duration']
+        if duration is None:
+            return 0
+        return duration / 2
 
-    html_content += '<div class="row">'
+def make_random_search_phrase(word_list):
+    words = random.sample(word_list, 2)
+    phrase = ' '.join(words)
+    print('Search phrase: "{}"'.format(phrase))
+    return phrase
 
-    for file in files:
-        file_name = file.split('-')[-1].split('.')[0]
-        file_path = f"wavs/processed/loop/{file}"
-        html_content += f'<div class="audio-file"><div>{file_name}</div><audio src="{file_path}"></audio></div>'
-        
-        file_count += 1
-        if file_count % 3 != 0:
-            html_content += ' '
-        if file_count % 3 == 0 and file_count != len(files):
-            html_content += '</div><div class="row">'
+def make_download_options(phrase):
+    safe_phrase = ''.join(x for x in phrase if x.isalnum() or x in "._- ")
+    return {
+        'format': 'bestaudio/best',
+        'paths': {'home': DOWNLOAD_DIR},
+        'outtmpl': {'default': f'{safe_phrase}-%(id)s.%(ext)s'},
+        'download_ranges': download_range_func(),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+        }]
+    }
 
-    html_content += '</div>'
-    return f'<div class="container">{html_content}</div>'
+def process_file(filepath, phrase):
+    try:
+        safe_phrase = ''.join(x for x in phrase if x.isalnum() or x in "._- ")
+        filename                 = os.path.basename(filepath)
+        output_filepath_oneshot  = os.path.join(ONESHOT_OUTPUT_DIR, f'oneshot_{safe_phrase}-{filename}')
+        output_filepath_loop     = os.path.join(LOOP_OUTPUT_DIR, f'loop_{safe_phrase}-{filename}')
 
-# Function to update HTML file
-def update_html_file(template_html_file_path, output_html_file_path, directory):
-    html_to_insert = generate_html_for_audio_files(directory)
-    with open(template_html_file_path, 'r') as template_file:
-        template_html_content = template_file.read()
-    modified_html_content = template_html_content.replace('<!-- Audio files will be added here -->', html_to_insert)
-    with open(output_html_file_path, 'w') as output_file:
-        output_file.write(modified_html_content)
+        sound = AudioSegment.from_file(filepath, "wav")
+        if len(sound) > 500:
+            if not os.path.exists(output_filepath_oneshot):
+                make_oneshot(sound, phrase, output_filepath_oneshot)
+            if not os.path.exists(output_filepath_loop):
+                make_loop(sound, phrase, output_filepath_loop)
+        os.remove(filepath)
+    except Exception as err:
+        print("Failed to process '{}' ({})".format(filepath, err))
 
-# Function to update the archive.html file
-def update_archive_html(archived_file_path):
-    archive_html_path = os.path.join(base_dir, 'archive.html')
-    link = f'<li><a href="{archived_file_path}">{archived_file_path.split("/")[-1]}</a></li>'
+def make_oneshot(sound, phrase, output_filepath):
+  final_length = min(2000, len(sound))
+  quarter = int(final_length/4)
+  sound   = sound[:final_length]
+  sound   = sound.fade_out(duration=quarter)
+  sound   = effects.normalize(sound)
+  sound.export(output_filepath, format="wav")
 
-    if not os.path.exists(archive_html_path):
-        with open(archive_html_path, 'w') as archive_file:
-            archive_file.write('<html><head><title>Sonic Garbage Archive</title></head><body>')
-            archive_file.write('<h1>Sonic Garbage Archive</h1><ul>')
-            archive_file.write(link)
-            archive_file.write('</ul></body></html>')
-    else:
-        with open(archive_html_path, 'r+') as archive_file:
-            content = archive_file.read()
-            position = content.find('</ul>')
-            content = content[:position] + link + content[position:]
-            archive_file.seek(0)
-            archive_file.write(content)
+def make_loop(sound, phrase, output_filepath):
+    final_length = min(2000, len(sound))
+    half         = int(final_length/2)
+    fade_length  = int(final_length/4)
+    beg   = sound[:half]
+    end   = sound[half:]
+    end   = end[:fade_length]
+    beg   = beg.fade_in(duration=fade_length)
+    end   = end.fade_out(duration=fade_length)
+    sound = beg.overlay(end)
+    sound = effects.normalize(sound)
+    sound.export(output_filepath, format="wav")
 
-# Function to archive existing index.html
-def archive_existing_index(output_html_file_path):
-    if os.path.exists(output_html_file_path):
-        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        archived_file_path = output_html_file_path.replace('index.html', f'index.{timestamp}.html')
-        os.rename(output_html_file_path, archived_file_path)
-        update_archive_html(archived_file_path)  # Update the archive.html
+def create_combined_loop():
+    combined_dir = os.path.join(base_dir, 'wavs/processed/combined')
+    os.makedirs(combined_dir, exist_ok=True)  # Create the combined directory if it doesn't exist
+
+    combined_loop = AudioSegment.silent(duration=100)  # A short silence to start
+    for filepath in glob.glob(os.path.join(LOOP_OUTPUT_DIR, '*.wav')):
+        sound = AudioSegment.from_file(filepath, format="wav")
+        repeated_sound = sound * random.randint(3, 4)  # Repeat 3 or 4 times
+        combined_loop += repeated_sound
+
+    version = 1
+    output_filename = "combined_loop_v{}.wav".format(version)
+    output_filepath = os.path.join(combined_dir, output_filename)
+
+    # Check if file already exists, and increment version number if it does
+    while os.path.exists(output_filepath):
+        version += 1
+        output_filename = "combined_loop_v{}.wav".format(version)
+        output_filepath = os.path.join(combined_dir, output_filename)
+
+    combined_loop.export(output_filepath, format="wav")
+
+def setup():
+    if not os.path.exists(LOOP_OUTPUT_DIR):
+        os.makedirs(LOOP_OUTPUT_DIR)
+    if not os.path.exists(ONESHOT_OUTPUT_DIR):
+        os.makedirs(ONESHOT_OUTPUT_DIR)
 
 # Main function where audio processing happens
 def main():
-    # Your existing main function code for audio processing
+    try:
+        setup()
+        word_list = read_lines(WORD_LIST)
+        for _ in range(BATCH_SIZE):
+            phrase = make_random_search_phrase(word_list)
+            video_url = f'ytsearch1:"{phrase}"'
+            options = make_download_options(phrase)
+            YoutubeDL(options).download([video_url])
+            for filepath in glob.glob(os.path.join(DOWNLOAD_DIR, f'{phrase}-*.wav')):
+                process_file(filepath, phrase)
+
+        create_combined_loop()  # Call this function to create the combined loop
+
+    except Exception as err:
+        print('FATAL ERROR: {}'.format(err))
 
     # Generate new index.html with updated audio files
-    archive_existing_index('/var/www/audio/index.html')  # Replace with your path
+    archive_existing_index('/var/www/audio/index.html')
     update_html_file('/var/www/audio/sonicgarbage_project/template_index.html', '/var/www/audio/index.html', loop_dir)
 
 # Flask route to trigger the main function
@@ -133,10 +181,14 @@ def run_script():
 # Flask route for the home page
 @app.route('/')
 def home():
+    # Call main function each time the home page is loaded
+    main()
+
     # Serve the updated index.html
     with open('/var/www/audio/index.html', 'r') as file:
         return file.read()
 
 # Run the Flask app if this script is run directly
 if __name__ == '__main__':
+    main()  # Automatically run main functionality
     app.run(debug=True)
